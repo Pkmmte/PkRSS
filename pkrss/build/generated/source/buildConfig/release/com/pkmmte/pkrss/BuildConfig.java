f65cbcb45aca5f952e25e4a2e6a9d01684e719bf
Pkmmte <pkx7225@gmail.com>
2014-08-20 19:56:31 -0700

    What is this?
    I didn't create any of this. D:

---
+++ pkrss/build/generated/source/buildConfig/release/com/pkmmte/pkrss/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.pkmmte.pkrss;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "com.pkmmte.pkrss";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 4;
+  public static final String VERSION_NAME = "1.1.1";
+}
